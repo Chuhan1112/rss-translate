239d79626e584e885e9d42275b65fb5375052e7e
Zhao Chuhan <35064712+Chuhan1112@users.noreply.github.com>
2024-06-11 14:52:30 +0800

    Update cron_random.py

--- .github/cron_random.py
+++ .github/cron_random.py
@@ -1,14 +1,21 @@
 # coding:utf-8 
 import random
-min=random.randint(0, 59)
-h=random.randint(0, 23)
 
+# 生成一个在上午的小时数和一个在下午的小时数
+h_morning = random.randint(0, 11)
+h_afternoon = random.randint(12, 23)
 
-YML=".github/workflows/circle_translate.yml"
+# 分钟数可以固定，也可以随机
+min_morning = random.randint(0, 59)
+min_afternoon = random.randint(0, 59)
+
+YML = ".github/workflows/circle_translate.yml"
 
 f = open(YML, "r+", encoding="UTF-8")
-list1 = f.readlines()           
-list1[7] = "   - cron: '%d %d * * *'\n"%(min,h)
+list1 = f.readlines()
+# 设置一个在上午，一个在下午的时间点
+list1[7] = "   - cron: '%d %d * * *'\n" % (min_morning, h_morning)
+list1[8] = "   - cron: '%d %d * * *'\n" % (min_afternoon, h_afternoon)
 
 f = open(YML, "w+", encoding="UTF-8")
 f.writelines(list1)
